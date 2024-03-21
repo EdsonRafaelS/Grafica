ddac11949deeeec09a3f18b802f9b310350168db
EdsonRafaelS <130865402+EdsonRafaelS@users.noreply.github.com>
2024-03-21 15:55:13 -0300

    Fim do Site/ Eu acho

--- js/script.js
+++ js/script.js
@@ -23,3 +23,54 @@ function enviarWhatsApp2() {
     var url = 'https://wa.me/' + numeroTelefone;
     window.open(url);
 }
+
+function rolarParaContato() {
+    var contatoSection = document.getElementById("Contato");
+    contatoSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function initCarousel() {
+    const prevButton = document.querySelector('.Carrossel-container .prev');
+    const nextButton = document.querySelector('.Carrossel-container .next');
+    const carousel = document.querySelector('.Carrossel-container .imagens');
+    const slides = carousel.querySelectorAll('img');
+  
+    let slideIndex = 0;
+  
+    function showSlide(index) {
+      // Esconda todos os slides
+      slides.forEach((slide) => {
+        slide.style.display = 'none';
+      });
+  
+      // Mostre o slide atual
+      slides[index].style.display = 'block';
+    }
+  
+    function nextSlide() {
+      slideIndex++;
+      if (slideIndex >= slides.length) {
+        slideIndex = 0; // Volta para o primeiro slide
+      }
+      showSlide(slideIndex);
+    }
+  
+    function prevSlide() {
+      slideIndex--;
+      if (slideIndex < 0) {
+        slideIndex = slides.length - 1; // Volta para o último slide
+      }
+      showSlide(slideIndex);
+    }
+  
+    // Adicione eventos de clique aos botões de navegação
+    prevButton.addEventListener('click', prevSlide);
+    nextButton.addEventListener('click', nextSlide);
+  
+    // Mostre o primeiro slide quando a página carregar
+    showSlide(slideIndex);
+  }
+  
+  // Chame a função de inicialização do carrossel quando o DOM estiver pronto
+  document.addEventListener('DOMContentLoaded', initCarousel);
+  
